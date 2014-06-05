2bb6b78d2d1ded3d4f45e9f0af37e2290565d2c3
schultzRr <rosas_schultz@hotmail.com>
2014-06-05 14:14:16 -0500

    Creación de la estructura general de todas las páginas de la aplicación

--- Carpooling/www/js/index.js
+++ Carpooling/www/js/index.js
@@ -33,17 +33,215 @@ var app = {
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+        StatusBar.overlaysWebView(false);
+                
+        $(document).on('pageinit', '#dashboard', function(){ 
+			getDashboard();
+			updateDashboard();
+		});
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+		$(document).on('pageinit', '#search-ride', function(){ 
+			getAllRides();
+			updateAllRides();
+		});
+		
+		$(document).on('pagebeforeshow', "#view-my-ride", function (event, data) {
+    		var rideId = sessionStorage.rideId;
+    		var ride = null;
+    		
+    		for(var i=0; i<myRides.length; i++) {
+				if(myRides[i].id == rideId) {
+					ride = myRides[i];
+				}
+			}
+			
+			if(ride != null) {
+				$('#view-my-ride .view-my-ride-from').html(ride.from);
+				$('#view-my-ride .view-my-ride-to').html(ride.to);
+				$('#view-my-ride .view-my-ride-time').html(ride.time);
+			} else {
+				$.mobile.changePage($('#dashboard'), {transition: 'slide'});
+				alert('There was an error retrieving the ride details.');
+			}
+		});
 
-        console.log('Received Event: ' + id);
+		$(document).on('pagebeforeshow', "#view-next-ride", function (event, data) {
+    		var rideId = sessionStorage.rideId;
+    		var ride = null;
+    		
+    		for(var i=0; i<nextRides.length; i++) {
+				if(nextRides[i].id == rideId) {
+					ride = nextRides[i];
+				}
+			}
+			
+			if(ride != null) {
+				$('#view-next-ride .view-next-ride-from').html(ride.from);
+				$('#view-next-ride .view-next-ride-to').html(ride.to);
+				$('#view-next-ride .view-next-ride-time').html(ride.time);
+			} else {
+				$.mobile.changePage($('#dashboard'), {transition: 'slide'});
+				alert('There was an error retrieving the ride details.');
+			}
+		});
+
+		$(document).on('pagebeforeshow', "#view-ride", function (event, data) {
+    		var rideId = sessionStorage.rideId;
+    		var ride = null;
+    		
+    		for(var i=0; i<allRides.length; i++) {
+				if(allRides[i].id == rideId) {
+					ride = allRides[i];
+				}
+			}
+			
+			if(ride != null) {
+				$('#view-ride .view-ride-from').html(ride.from);
+				$('#view-ride .view-ride-to').html(ride.to);
+				$('#view-ride .view-ride-time').html(ride.time);
+			} else {
+				$.mobile.changePage($('#dashboard'), {transition: 'slide'});
+				alert('There was an error retrieving the ride details.');
+			}
+		});
+
+		$(document).on('click', '#list-my-rides li', function(event, ui) {
+			if(!$(this).attr('data-role')) {
+				sessionStorage.rideId = $(this).attr('ride-id');
+				$.mobile.changePage($('#view-my-ride'), {transition: 'slide'}); 
+			}
+		});
+		
+		$(document).on('click', '#list-next-rides li', function(event, ui) {
+			if(!$(this).attr('data-role')) {
+				sessionStorage.rideId = $(this).attr('ride-id');
+				$.mobile.changePage($('#view-next-ride'), {transition: 'slide'}); 
+			}
+		});
+
+		$(document).on('click', '#list-all-rides li', function(event, ui) {
+			if(!$(this).attr('data-role')) {
+				sessionStorage.rideId = $(this).attr('ride-id');
+				$.mobile.changePage($('#view-ride'), {transition: 'slide'}); 
+			}
+		});
+		
+		$('#btn-add-ride').click(function() {
+			myRides.push({'from':'New Ride', 'to':'Mi casa'});
+			updateDashboard();
+		    $.mobile.changePage($('#dashboard'), {transition: 'slide', reverse: 'true'}); 
+		});
+		
     }
 };
+
+
+var myRides = new Array();
+var nextRides = new Array();
+var allRides = new Array();
+
+function showLoader() {
+	$.mobile.loading('show');
+}
+
+function hideLoader() {
+	$.mobile.loading('hide');
+}
+
+function getDashboard() {
+	showLoader();
+	myRides = [
+		{
+			'id': 1,
+			'from': 'Mi casa', 
+			'to': 'Tu casa',
+			'time': '12:00'
+		}
+	];
+	nextRides = [
+		{
+			'id': 2,
+			'from': 'Mi casa', 
+			'to': 'Tu casa',
+			'time': '8:00'
+		}
+	];
+}
+
+function updateDashboard() {
+	clearDashboard();
+	var myRidesList = $('#list-my-rides');
+	
+	myRidesList.append($('<li data-role="list-divider" >Mis viajes publicados</li>'));
+	for(var i=0; i<myRides.length; i++) {
+		html = '<li ride-id="' + myRides[i].id + '"><a>' + myRides[i].from + '</a></li>';
+		myRidesList.append($(html));
+	}
+	
+	myRidesList.listview('refresh');
+	
+	var nextRidesList = $('#list-next-rides');
+	
+	nextRidesList.append($('<li data-role="list-divider" >Viajes a los que apliqu&eacute;</li>'));
+	for(var i=0; i<nextRides.length; i++) {
+		html = '<li ride-id="' + nextRides[i].id + '"><a>' + nextRides[i].from + '</a></li>';
+		nextRidesList.append($(html));
+	}
+	
+	nextRidesList.listview('refresh');
+	
+	setTimeout(function(){hideLoader();}, 2000);
+}
+
+function clearDashboard() {
+	var myRidesList = $('#list-my-rides');
+	var nextRidesList = $('#list-next-rides');
+	
+	myRidesList.html('');
+	nextRidesList.html('');
+}
+
+function getAllRides() {
+	showLoader();
+	allRides = [
+		{
+			'id': 1,
+			'from': 'Mi casa', 
+			'to': 'Tu casa',
+			'time': '12:00'
+		},
+		{
+			'id': 2,
+			'from': 'Mi casa', 
+			'to': 'Tu casa',
+			'time': '21:00'
+		},
+		{
+			'id': 3,
+			'from': 'Mi casa', 
+			'to': 'Tu casa',
+			'time': '2:00'
+		}
+	];
+}
+
+function updateAllRides() {
+	clearAllRides();
+	var allRidesList = $('#list-all-rides');
+	
+	allRidesList.append($('<li data-role="list-divider" >Todos los viajes</li>'));
+	for(var i=0; i<allRides.length; i++) {
+		html = '<li ride-id="' + allRides[i].id + '"><a>' + allRides[i].from + '</a></li>';
+		allRidesList.append($(html));
+	}
+	
+	allRidesList.listview('refresh');
+	
+	hideLoader();
+}
+
+function clearAllRides() {
+	var allRidesList = $('#list-all-rides');
+
+	allRidesList.html('');
+}
