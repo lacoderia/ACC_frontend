d97982a930126f64121386beef976df75072a50e
schultzRr <rosas_schultz@hotmail.com>
2014-06-13 20:12:25 -0500

    Se agregaron llamadas a los servicios de dashboard, detalle del viaje y agregar viaje

--- ACC/platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-C:\Users\Alberto\Documents\in2teck\ACC\ACC_frontend\ACC\platforms\android\CordovaLib\ant-build\AndroidManifest.xml : \
-C:\Users\Alberto\Documents\in2teck\ACC\ACC_frontend\ACC\platforms\android\CordovaLib\AndroidManifest.xml \
--- ACC/platforms/android/ant-build/HelloWorld-debug-unaligned.apk.d
+++
@@ -1,4 +0,0 @@
-C:\Users\Alberto\Documents\in2teck\ACC\ACC_frontend\ACC\platforms\android\ant-build\HelloWorld-debug-unaligned.apk : \
-C:\Users\Alberto\Documents\in2teck\ACC\ACC_frontend\ACC\platforms\android\ant-build\HelloWorld.ap_ \
-C:\Users\Alberto\Documents\in2teck\ACC\ACC_frontend\ACC\platforms\android\ant-build\classes.dex \
-C:\Users\Alberto\Documents\in2teck\ACC\ACC_frontend\ACC\platforms\android\CordovaLib\ant-build\classes.jar \
