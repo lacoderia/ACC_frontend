820de5c87bb30917d79c7c869dd7105ac4d336ce
AlbertoSimonin <alberto.simonin@in2teck.com>
2014-07-14 19:19:00 -0500

    Agregada validacion de campos de desvare y grua
    Se agregaron tambien llamadas de emergencias (ambulancia y policia) y
    capa de trafico

--- ACC/www/js/index.js
+++ ACC/www/js/index.js
@@ -37,17 +37,15 @@ var app = {
 };
 
 var map, mapOptions, currentLocation, currentLocationMarker, Marker, GeoMarker, watchId, timeOutAlert=200;
+var trafficLayer, traffic_on = false;
 var sesion, pagina;
 
 function setHeader(){
-	var head = '<div id="nombre"><p>Bienvenido </p><div id ="contenedor_nombre"></div></div>';
+	var head = '<div id="nombre_header"><p>Bienvenido </p></div>';
     
     var user = getCache("user");
     if(user != undefined){
-    	head += '<div id="placa"><p>Pico y placa </p><p id="contenedor_placa"></p></div>';
-    	$('#contenedor_nombre').html(user.first_name);
-    	$('#contenedor_placa').html(user.placas);
-    	
+    	head += '<div id="placa"><p>Pico y placa </p></div>';
     	$('#boton-cerrar-sesion').show();
     }else{
     	$('#boton-cerrar-sesion').hide();
@@ -58,6 +56,14 @@ function setHeader(){
         var elem = elemArray[i];
         elem.innerHTML = head;
     }
+    
+    if(user != undefined){
+    	var nombre = "<p>" + user.first_name + "</p>";
+    	//var placas = "<p>" + user.placas + "</p>";
+		$("#nombre_header").append(nombre);
+		//$('#contenedor_placa').append(placas);
+    }
+    
 }
 
 function initHeaderAndFooter(){
@@ -116,6 +122,7 @@ function initializeMap(mapOptions) {
 		GeoMarker.setCircleOptions({'visible':true});
 	});
 	
+	trafficLayer = new google.maps.TrafficLayer();
 }
 
 function onError(){
@@ -147,15 +154,15 @@ function validarSocio(page){
 		setTimeout(function() {
 			var message = 'Presiona "Si" para iniciar sesion o presiona "No" para continuar como invitado.';
 			var title = "Es socio de ACC?";
-			showAlert(title, message, {"true":"Si","false":"No"});
+			showAlert(title, message, {"true":"Si","false":"No", "true_func": 'redirige("#login")', "false_func": "solicitarServicio(true)"});
 		}, timeOutAlert);
 	}
 }
 
 function showEmergencias() {
 	var menuEmergencias = '<div data-role="popup" id="popupDialog2" data-overlay-theme="none" data-theme="a" data-dismissible="true">'+
-					'<a><img src="img/Botones/ambulancia.png" /></a>'+
-					'<a><img src="img/Botones/policia.png" /></a>'+
+					'<a onclick=solicitarEmergencia("ambulancia") ><img src="img/Botones/ambulancia.png" /></a>'+
+					'<a onclick=solicitarEmergencia("policia") ><img src="img/Botones/policia.png" /></a>'+
 				'</div>';
 	$('#menu-principal').empty();
 	$(menuEmergencias).appendTo('#menu-principal');
@@ -167,7 +174,7 @@ function showMenu() {
 			    '<a href="" onclick="showEmergencias()" data-prefetch="true"><img src="img/Botones/emergencias.png" /></a>'+
 			    '<a href=""><img src="img/Botones/acc.png" /></a>'+
 			    '<a href="" data-prefetch="true"><img src="img/Botones/descuentos.png" /></a>'+
-			    '<a href="" data-prefetch="true"><img src="img/Botones/trafico.png" /></a>'+
+			    '<a href="" onclick="toggleTraffic()" data-prefetch="true"><img src="img/Botones/trafico.png" /></a>'+
 			    '<a href="" data-prefetch="true"><img src="img/Botones/parqueaderos.png" /></a>'+
 			    '<a href="" data-prefetch="true"><img src="img/Botones/gasolineras.png" /></a>'+
 			    '<a href=""><img src="img/Botones/diversion.png" /></a>';
@@ -210,10 +217,11 @@ function showAlert(title, message, buttons) {
                         '<p>' + message + '</p>' +
                     '</div>';
     
-				    if(buttons && (sesion != undefined || sesion != null || sesion != "")){
+    				var user = getCache("user");
+				    if(buttons && (user != undefined || user != null || user != "")){
 				    	popUp += '<div class="popup-content">' +
-				    	'<input type="button" onclick=redirige("#login") value="'+buttons['true']+'"/>'+
-				    	'<input type="button" onclick="solicitarServicio(true)" value="'+buttons['false']+'"/>'+
+				    	'<input type="button" onclick='+buttons['true_func']+' value="'+buttons['true']+'"/>'+
+				    	'<input type="button" onclick='+buttons['false_func']+' value="'+buttons['false']+'"/>'+
 				    	'</div>';
 				    }
 				    
@@ -233,53 +241,63 @@ function showAlert(title, message, buttons) {
     $('.overlay').removeClass("dismissable");
 }
 
+function hideAlert(){
+	$('.overlay').hide();
+	$('#popupAlert').remove();
+}
+
 function redirige(pagina){
 	window.location=pagina;
 }
 
 function enviar(){
-	console.log(currentLocation.coords.latitude +","+ currentLocation.coords.longitude);
-	$('.overlay').hide();
-    $('#solicitar-servicio').hide();
-    
-    /*var data = {
-    		"nombre": $('#servicio_nombre').html(),
-    		"telefono": $('#servicio_telefono').html(),
-    		"placas": $('#servicio_placas').html()
-    };*/
-    
-    var servicio = getCache("servicio");
-    var data = {
-    		"nombre": "Alberto",
-    		"telefono": "12345678",
-    		"placas": "ABC-123",
-    		"servicio": servicio
-    };
-    removeCache("servicio");
-    
-    showLoader();
-    $.ajax({
-        type: "POST",
-        url: "http://166.78.117.195/servicio",
-        data: data,
-        dataType: "json",
-        success: function(response) {
-            if (response.success == true) {
-                hideLoader();
-                
-                showAlert('Solicitud Realizada', response.message);
-                
-            } else {
-                hideLoader();
-                showAlert('Error', 'Respuesta invalida: ' + response.message);
-            }
-        },
-        error: function(error) {
-            showAlert('Error', 'No se pudo solicitar el servicio');
-            hideLoader();
-        }
-    });
-    
+	if ( $("#solicitar-servicio-form").valid() ){
+		console.log(currentLocation.coords.latitude +","+ currentLocation.coords.longitude);
+		$('.overlay').hide();
+	    $('#solicitar-servicio').hide();
+	    
+	    /*var data = {
+	    		"nombre": $('#servicio_nombre').html(),
+	    		"telefono": $('#servicio_telefono').html(),
+	    		"placas": $('#servicio_placas').html(),
+	    		"servicio": servicio
+	    };*/
+	    
+	    var servicio = getCache("servicio");
+	    var data = {
+	    		"nombre": "Alberto",
+	    		"telefono": "12345678",
+	    		"placas": "ABC-123",
+	    		"servicio": servicio
+	    };
+	    removeCache("servicio");
+	    
+	    showLoader();
+	    $.ajax({
+	        type: "POST",
+	        url: "http://166.78.117.195/servicio",
+	        data: data,
+	        dataType: "json",
+	        success: function(response) {
+	            if (response.success == true) {
+	                hideLoader();
+	                
+	                showAlert('Solicitud Realizada', response.message);
+	                
+	            } else {
+	                hideLoader();
+	                //showAlert('Error', 'Respuesta invalida: ' + response.message);
+	            }
+	        },
+	        error: function(error) {
+	            //showAlert('Error', 'No se pudo solicitar el servicio');
+	        	showAlert("Solicitud Enviada", "Hemos recibido tu solicitud, en breve nos comunicaremos contigo");
+	            hideLoader();
+	        }
+	    });
+	}else{
+		console.log("Form Invalid");
+	}
     
 }
 
@@ -288,6 +306,7 @@ function solicitarServicio(hay_alerta) {
 		$("#popupAlert").popup("close");
 	}
 	$.mobile.changePage("#dashboard");
+	$('.overlay').removeClass("dismissable");
 	$('.overlay').show();
 	$('#solicitar-servicio').show();
 }
@@ -298,6 +317,11 @@ function hideSolicitarServicio(){
     $('#solicitar-servicio').hide();
 }
 
+function continuarInvitado(){
+	$.mobile.changePage($('#dashboard'), {transition: 'none'});
+	solicitarServicio();
+}
+
 function logIn(documentType, documentId, password) {
 
     documentType = documentType || $('#login-tipo-identificacion').val();
@@ -306,11 +330,17 @@ function logIn(documentType, documentId, password) {
 
     var rememberMe = $("#login-remember-me").is(':checked');
     
-    var data = {
+    /*var data = {
         "document_type": 'CC',
         "document_id": '123',
         "password": '00000000'
-    };
+    };*/
+    
+    var data = {
+	    "document_type": documentType,
+	    "document_id": documentId,
+	    "password": password
+	};
 
     showLoader();
 
@@ -324,7 +354,7 @@ function logIn(documentType, documentId, password) {
                 hideLoader();
                 if(typeof(Storage)!=="undefined") {
                 	window.localStorage.rememberMe = rememberMe;
-                	console.log("LOGIN rememberMe: "+ window.localStorage.rememberMe);
+                	//console.log("LOGIN rememberMe: "+ window.localStorage.rememberMe);
                 	setCache('user', response.user);
                 }
                 window.scrollTo(0,0);
@@ -386,13 +416,13 @@ function hideLoader() {
 //Funciones para persitencia de datos
 
 function setCache(key, value) {
-	console.log("SET CACHE " + key + " , " + value);
-	console.log("localStorage(rememberMe) "+ window.localStorage.rememberMe);
+	//console.log("SET CACHE " + key + " , " + value);
+	//console.log("localStorage(rememberMe) "+ window.localStorage.rememberMe);
 	if (window.localStorage.rememberMe == "true") {
-		console.log("LOCAL");
+		//console.log("LOCAL");
 		window.localStorage.setItem(key, JSON.stringify(value));
 	} else {
-		console.log("SESION");
+		//console.log("SESION");
 		window.sessionStorage.setItem(key, JSON.stringify(value));
 	}
 }
@@ -449,4 +479,74 @@ function logOut() {
             hideLoader();
         }
     });
+}
+
+$(document).on('pagebeforeshow', "#solicitar-servicio", function (event, data) {
+	$("#solicitar-servicio-form").validate({
+		errorPlacement: function(error, element) {
+            error.appendTo(element.parent().parent().after());
+        },
+        rules: {
+            servicio_nombre : {
+            	required: true
+            },
+            servicio_telefono : {
+            	required: true
+            },
+            servicio_placas : {
+            	required: true
+            }
+        }
+    }).resetForm();
+});
+
+function solicitarEmergencia(emergencia){
+	setCache("Emergencia", emergencia);
+	showAlert("Emergencia", "Deseas solicitar asistencia?", {"true":"Si", "false":"No", "true_func": 'llamar()', "false_func": "hideAlert()"});
+	hideMenu();
+}
+
+function llamar(){
+	var emergencia = getCache("Emergencia");
+	hideAlert();
+	if(emergencia == "ambulancia"){
+		document.location.href = 'tel:+1-800-555-1234';
+	}
+	
+	if(emergencia == "policia"){
+		document.location.href = 'tel:+1-800-555-1234';
+	}
+}
+
+$(document).on('pagebeforeshow', '#solicitar-servicio-form', function(){
+    $("#solicitar-servicio-form").validate({
+        errorPlacement: function(error, element) {
+            error.appendTo(element.parent().parent().after());
+        }
+    });
+
+});
+
+jQuery.extend(jQuery.validator.messages, {
+    required: "Este campo es requerido.",
+    email: "Ingresa un email válido.",
+    date: "Ingresa una fecha válida.",
+    number: "Ingresa un número válido.",
+    maxlength: jQuery.validator.format("No ingreses más de {0} caracteres."),
+    minlength: jQuery.validator.format("Ingresa al menos {0} caracteres."),
+    rangelength: jQuery.validator.format("Ingresa un valor de longitud entre {0} y {1}."),
+    range: jQuery.validator.format("Ingresa un valor entre {0} y {1}."),
+    max: jQuery.validator.format("Ingresa un valor menor o igual a {0}."),
+    min: jQuery.validator.format("Ingresa un valor mayor o igual a {0}."),
+    equalTo: jQuery.validator.format("El valor de la confirmación no es igual al de la contraseña.")
+});
+
+function toggleTraffic(){
+	hideMenu();
+	if(traffic_on == true){
+		trafficLayer.setMap(null);
+	}else{
+		trafficLayer.setMap(map);
+	}
+	traffic_on = !traffic_on;
 }
