eb07574f28aaa941c85775a0f35b0f150de7f4ea
AlbertoSimonin <alberto.simonin@in2teck.com>
2014-07-17 19:18:00 -0500

    Corregidos estilos rotos. Add pantalla Mi Cuenta

--- ACC/www/js/index.js
+++ ACC/www/js/index.js
@@ -33,7 +33,7 @@ var app = {
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicity call 'app.receivedEvent(...);'
     onDeviceReady: function() {
-        navigator.splashscreen.hide();
+        //navigator.splashscreen.hide();
         showLoader();
 
         navigator.geolocation.getCurrentPosition(onSuccess, onError);
