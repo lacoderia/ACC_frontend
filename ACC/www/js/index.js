14bb325ddc8a21b36bb4935f60269bd4794f102e
schultzRr <rosas_schultz@hotmail.com>
2014-07-24 12:42:56 -0500

    ACC: Se modificaron los estilos para que se ajuste a multiples dispositivos móviles

--- ACC/www/js/index.js
+++ ACC/www/js/index.js
@@ -33,17 +33,14 @@ var app = {
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicity call 'app.receivedEvent(...);'
     onDeviceReady: function() {
-        //navigator.splashscreen.hide();
         showLoader();
 
+        document.addEventListener("backbutton", onBackKeyDown, false);
+        function onBackKeyDown(e) {
+            e.preventDefault();
+        }
+
         navigator.geolocation.getCurrentPosition(onSuccess, onError);
-        
-        document.addEventListener("deviceready", onDeviceReady, false);
-	        function onDeviceReady() {
-	            document.addEventListener("backbutton", function (e) {
-	                e.preventDefault();
-	            }, false );
-	    };
 
         $('.acc-btn-back').click(function() {
             window.scrollTo(0,0);
