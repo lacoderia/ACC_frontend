a3d70086f22ce3a9a34a21653ffd381864191184
schultzRr <ZFja5Hdb>
2015-04-16 15:50:33 -0500

    .gitignore is now working

--- Carpooling/platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-C:\Users\Alberto\Documents\in2teck\ACC\ACC_frontend\Carpooling\platforms\android\CordovaLib\ant-build\AndroidManifest.xml : \
-C:\Users\Alberto\Documents\in2teck\ACC\ACC_frontend\Carpooling\platforms\android\CordovaLib\AndroidManifest.xml \
--- Carpooling/platforms/android/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-C:\Users\Alberto\Documents\in2teck\ACC\ACC_frontend\Carpooling\platforms\android\ant-build\AndroidManifest.xml : \
-C:\Users\Alberto\Documents\in2teck\ACC\ACC_frontend\Carpooling\platforms\android\AndroidManifest.xml \
